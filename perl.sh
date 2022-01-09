824d4f686d7a35b6afcb5bb4c2c14e0c7cc6f30c
hf-bell <71832427+hf-bell@users.noreply.github.com>
2022-01-09 19:31:12 +0000

    Add files via upload

---
+++ perl.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+###########
+# USAGE NOTE:
+# The following script is designed to help you get started running models with the codebase.
+# At a minimum, all you need to do is give a name do your experiment using the ${EXP_NAME} variable.
+# Additional arguments can be added to the train.py command for different functionality.
+# We recommend copying this script and modifying it for each experiment you try (multi-layer, lexical etc)
+###########
+
+# Define a location for all your experiments to save
+ROOT=$(git rev-parse --show-toplevel)
+RESULTS_ROOT="${ROOT}/results"
+mkdir -p ${RESULTS_ROOT}
+
+### NAME YOUR EXPERIMENT HERE ##
+EXP_NAME="attention2"
+################################
+
+## Local variables for current experiment
+EXP_ROOT="${RESULTS_ROOT}/${EXP_NAME}"
+DATA_DIR="${ROOT}/europarl_prepared"
+TEST_EN_GOLD="${ROOT}/europarl_raw/test.en"
+TEST_EN_PRED="${EXP_ROOT}/model_translations.txt"
+mkdir -p ${EXP_ROOT}
+
+
+## Calculate BLEU score for model outputs
+perl multi-bleu.perl -lc ${TEST_EN_GOLD} < ${TEST_EN_PRED} | tee "${EXP_ROOT}/bleu.txt"
